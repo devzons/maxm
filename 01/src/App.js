d891038ce6f6c87ac67082b73e3b62c009d116e5
devzons <devzons@gmaill.com>
2019-01-14 17:58:53 -0800

    first commit

---
+++ 01/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import UserInput from './components/UserInput'
+import UserOutput from './components/UserOutput'
+import './App.css'
+
+class App extends Component {
+  state = {username: 'janedoe'}
+
+  changeUsernameHandler = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <UserInput changed={this.changeUsernameHandler} />
+        <UserOutput 
+          username={this.state.username} 
+          text="Hello, this is my text one!" />
+      </div>
+    );
+  }
+}
+
+export default App;
